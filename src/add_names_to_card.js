28e2530e266342a7b736b305576eda13e8fc5c3e
Rahul Anand <rahul@canva.com>
2021-09-16 12:34:40 +1000

    fixes

---
+++ src/add_names_to_card.js
@@ -0,0 +1,5 @@
+import * as cardImg from "./local-only/invitation.png";
+
+export function addNamesToCard() {
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./index.html";
 import "./base.css";
 import "./style.css";
+import { addNamesToCard } from "./add_names_to_card";
+
+addNamesToCard();
