63e0af0fe558e1333a6ef40423b0615a99ec56c1
Rahul Anand <rahul@canva.com>
2021-09-23 11:47:32 +1000

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import "./index.html";
 import "./base.css";
 import "./style.css";
-import { addNamesToCard } from "./add_names_to_card";
 import { initUi } from "./ui";
 import { config } from "./config";
 
-// addNamesToCard();
 initUi(config);
